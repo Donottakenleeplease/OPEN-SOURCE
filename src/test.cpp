93e0861aad1ceeebdc37b17008142a60debb00c2
lee <517114237@qq.com>
2019-09-19 10:25:28 +0800

    v2

--- src/test.cpp
+++ src/test.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
  
 #include "mongoose.h"
+#include "base64.h"
  
 using namespace std;
  
@@ -28,12 +29,17 @@ int env_handler(struct mg_connection *conn)
 {
   static int counter = 0;
   counter++;
-  printf("counter: %d\n", counter);
- 
-  const char * str_data = conn->content;  // 服务端收到的消息
-  int str_len = conn->content_len;        // 服务端收到的消息长度
-  string str(str_data, str_len);
-  mg_printf(conn, "Received: %s, %d", str.c_str(), counter);
-  
+
+  const char* encoded_data = conn->content;  // 服务端收到的消息
+  int encoded_len = conn -> content_len;
+  string str_encoded(encoded_data,encoded_len);
+  printf("counter:%d,%s\n",counter,str_encoded.c_str());
+
+  vector<BYTE> str_decoded_byte = base64_decode(str_encoded);
+  int decoded_len = str_decoded_byte.size();
+  string str_decoded;
+  str_decoded.assign(str_decoded_byte.begin(),str_decoded_byte.end());
+  mg_printf(conn,"Received:%s,%d",str_decoded.c_str(),counter);
+
   return 0;
 }
