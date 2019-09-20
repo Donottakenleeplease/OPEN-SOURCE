e664d5adacb5e4d32135ba43ba2e4f0dfdbd8120
lee <517114237@qq.com>
2019-09-20 10:35:20 +0800

    Yolo_Sever

--- src/Sever_Yolo.cpp
+++ src/Sever_Yolo.cpp
@@ -77,113 +77,119 @@ int env_handler(struct mg_connection *conn)
           classNamesVec.push_back(className);
   }
 
-  bool stop=false;
+  /******* 开始网络推理 ********/
 
-  while(!stop)
+  cvtColor(mat, rgbImg, cv::COLOR_BGR2RGB);
+
+  float* srcImg;
+  size_t srcSize=rgbImg.rows*rgbImg.cols*3*sizeof(float);
+  srcImg=(float*)malloc(srcSize);
+
+  imgConvert(rgbImg,srcImg);//将图像转为yolo形式
+
+  float* resizeImg;
+  size_t resizeSize=net->w*net->h*3*sizeof(float);
+  resizeImg=(float*)malloc(resizeSize);
+  imgResize(srcImg,resizeImg,mat.cols,mat.rows,net->w,net->h);//缩放图像
+
+  network_predict(net,resizeImg);//网络推理
+  int nboxes=0;
+  detection *dets=get_network_boxes(net,rgbImg.cols,rgbImg.rows,thresh,0.5,0,1,&nboxes);
+
+  if(nms)
   {
-    cvtColor(mat, rgbImg, cv::COLOR_BGR2RGB);
-
-    float* srcImg;
-    size_t srcSize=rgbImg.rows*rgbImg.cols*3*sizeof(float);
-    srcImg=(float*)malloc(srcSize);
-
-    imgConvert(rgbImg,srcImg);//将图像转为yolo形式
-
-    float* resizeImg;
-    size_t resizeSize=net->w*net->h*3*sizeof(float);
-    resizeImg=(float*)malloc(resizeSize);
-    imgResize(srcImg,resizeImg,mat.cols,mat.rows,net->w,net->h);//缩放图像
-
-    network_predict(net,resizeImg);//网络推理
-    int nboxes=0;
-    detection *dets=get_network_boxes(net,rgbImg.cols,rgbImg.rows,thresh,0.5,0,1,&nboxes);
-
-    if(nms)
-    {
-      do_nms_sort(dets,nboxes,classes,nms);
-    }
-    vector<cv::Rect>boxes;
-    boxes.clear();
-    vector<int>classNames;
-
-    for (int i = 0; i < nboxes; i++)
-    {
-      bool flag=0;
-      int className;
-      for(int j=0;j<classes;j++)
-      {
-        if(dets[i].prob[j]>thresh)
-        {
-          if(!flag)
-          {
-            flag=1;
-            className=j;
-          }
-        }
-      }
-      if(flag)
-      {
-        int left = (dets[i].bbox.x - dets[i].bbox.w / 2.)*mat.cols;
-        int right = (dets[i].bbox.x + dets[i].bbox.w / 2.)*mat.cols;
-        int top = (dets[i].bbox.y - dets[i].bbox.h / 2.)*mat.rows;
-        int bot = (dets[i].bbox.y + dets[i].bbox.h / 2.)*mat.rows;
-
-        if (left < 0)
-        {
-          left = 0;
-        }
-        if (right > mat.cols - 1)
-        {
-          right = mat.cols - 1;
-        }
-      
-        if (top < 0)
-        {
-          top = 0;
-        }
-        if (bot > mat.rows - 1)
-            bot = mat.rows - 1;
-
-        Rect box(left, top, fabs(left - right), fabs(top - bot));
-        boxes.push_back(box);
-        classNames.push_back(className);
-      }
-    }
-
-    free_detections(dets, nboxes);
- 
-    for(int i=0;i<boxes.size();i++)
-    {
-      int offset = classNames[i]*123457 % 80;
-      float red = 255*get_color(2,offset,80);
-      float green = 255*get_color(1,offset,80);
-      float blue = 255*get_color(0,offset,80);
-
-      rectangle(mat,boxes[i],Scalar(blue,green,red),2);
-
-      String label = String(classNamesVec[classNames[i]]);
-      int baseLine = 0;
-      Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
-      putText(mat, label, Point(boxes[i].x, boxes[i].y + labelSize.height),
-              FONT_HERSHEY_SIMPLEX, 1, Scalar(red, blue, green),2);
-    }
-    imshow("video",mat);
-
-    int c=waitKey(30);
-          
-    if((char)c==27)
-    {
-      break;
-    }
-    else if( c >= 0 )
-    {
-      waitKey(0);
-    }
-    free(srcImg);
-    free(resizeImg);
+    do_nms_sort(dets,nboxes,classes,nms);
   }
+  vector<cv::Rect>boxes;
+  boxes.clear();
+  vector<int>classNames;
+
+  string detect_result = ""; //构造传输
+  
+  for (int i = 0; i < nboxes; i++) //检测classname与像素坐标
+  {
+    bool flag=0;
+    int className;
+
+    if(!detect_result.empty())
+    {
+      detect_result += " ";
+    }//构造传输的string数据
+
+    for(int j=0;j<classes;j++)
+    {
+      if(dets[i].prob[j]>thresh)
+      {
+        if(!flag)
+        {
+          flag=1;
+          className=j;//只检测人则改为0
+        }
+      }
+    }
+    if(flag)
+    {
+      int left = (dets[i].bbox.x - dets[i].bbox.w / 2.)*mat.cols;
+      int right = (dets[i].bbox.x + dets[i].bbox.w / 2.)*mat.cols;
+      int top = (dets[i].bbox.y - dets[i].bbox.h / 2.)*mat.rows;
+      int bot = (dets[i].bbox.y + dets[i].bbox.h / 2.)*mat.rows;
+
+      if (left < 0)
+      {
+        left = 0;
+      }
+      if (right > mat.cols - 1)
+      {
+        right = mat.cols - 1;
+      }
+    
+      if (top < 0)
+      {
+        top = 0;
+      }
+      if (bot > mat.rows - 1)
+          bot = mat.rows - 1;
+
+      Rect box(left, top, fabs(left - right), fabs(top - bot));
+      boxes.push_back(box);
+      classNames.push_back(className);
+
+      char ptr_result[30]; // 传输数据构建
+      sprintf(ptr_result,"%d %d %d %d",(int)left,(int)top,(int)right,(int)bot);
+      string str_result(ptr_result);
+      detect_result += str_result;
+    }
+  }
+
+  free_detections(dets, nboxes); 
+  
+  for(int i=0;i<boxes.size();i++)
+  {
+
+    int offset = classNames[i]*123457 % 80;
+    float red = 255*get_color(2,offset,80);
+    float green = 255*get_color(1,offset,80);
+    float blue = 255*get_color(0,offset,80);
+
+    rectangle(mat,boxes[i],Scalar(blue,green,red),2);
+
+    String label = String(classNamesVec[classNames[i]]);
+    int baseLine = 0;
+    Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+    putText(mat, label, Point(boxes[i].x, boxes[i].y + labelSize.height),
+            FONT_HERSHEY_SIMPLEX, 1, Scalar(red, blue, green),2);
+
+  }
+  imshow("video",mat);
+
+  free(srcImg);
+  free(resizeImg);
   free_network(net);
 
-//  mg_printf(conn,"Received:%s,%d",str_decoded.c_str(),counter);
+  printf("Counter: %3d, BBOX: %s, Time of Detect: %f\n", counter, 
+                                                         detect_result.empty() ? "Null" : detect_result.c_str(), 
+                                                         (double)((end.tv_sec - start.tv_sec)*1000.0 + (end.tv_usec - start.tv_usec)/1000.0));
+  mg_printf(conn, "%s", detect_result.c_str());
+
   return 0;
 }
